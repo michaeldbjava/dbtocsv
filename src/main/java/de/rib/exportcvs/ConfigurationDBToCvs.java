abca0ebbe33c30d94d9eab1494ecb6efe91f0769
michaeldbjava <michael-laube@web.de>
2016-11-26 14:00:28 +0100

    first check in of project files

---
+++ src/main/java/de/rib/exportcvs/ConfigurationDBToCvs.java
@@ -0,0 +1,196 @@
+/**
+ * 
+ */
+package de.rib.exportcvs;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+
+/**
+ * @author michael
+ *
+ */
+public class ConfigurationDBToCvs {
+	private String csvfile;
+	private String localhost;
+	private String database;
+	private String dbtype;
+	private String port;
+	private String user;
+	private String password;
+	private String sqlStatement;
+	private char delimeter;
+	private Connection conToDb = null;
+
+	/**
+	 * 
+	 */
+	public ConfigurationDBToCvs() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getCsvfile() {
+		return csvfile;
+	}
+
+	public void setCsvfile(String csvfile) {
+		this.csvfile = csvfile;
+	}
+
+	public String getLocalhost() {
+		return localhost;
+	}
+
+	public void setLocalhost(String localhost) {
+		this.localhost = localhost;
+	}
+
+	public String getDatabase() {
+		return database;
+	}
+
+	public void setDatabase(String database) {
+		this.database = database;
+	}
+
+	public String getDbtype() {
+		return dbtype;
+	}
+
+	public void setDbtype(String dbtype) {
+		this.dbtype = dbtype;
+	}
+
+	public String getPort() {
+		return port;
+	}
+
+	public void setPort(String port) {
+		this.port = port;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getSqlStatement() {
+		return sqlStatement;
+	}
+
+	public void setSqlStatement(String sqlStatement) {
+		this.sqlStatement = sqlStatement;
+	}
+
+	public char getDelimeter() {
+		return delimeter;
+	}
+
+	public void setDelimeter(char delimeter) {
+		this.delimeter = delimeter;
+	}
+	
+	
+	public boolean readConfigFile(String pathXMLConfigFile) {
+		try {
+			File xmlFile = new File(pathXMLConfigFile);
+			
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder documentBuilder = dbFactory.newDocumentBuilder();
+			Document document = documentBuilder.parse(xmlFile);
+			
+			
+			
+			
+
+			
+			this.setCsvfile(getValueOfXMLNode(document,"csv-file-to-export"));
+			this.setDelimeter(getValueOfXMLNode(document,"delimeter").charAt(0));
+			this.setDbtype(getValueOfXMLNode(document,"dbtype"));
+			this.setLocalhost(getValueOfXMLNode(document,"host"));
+			this.setDatabase(getValueOfXMLNode(document,"database_name"));
+			this.setUser(getValueOfXMLNode(document,"user"));
+			this.setPassword(getValueOfXMLNode(document,"password"));
+			this.setPort(getValueOfXMLNode(document,"port"));
+			this.setSqlStatement(getValueOfXMLNode(document,"sql-query"));
+			
+
+			
+			
+			System.out.println("Datenbank aus XML Datei: " + this.getDatabase());
+			System.out.println("Datenbank Typ aus XML Datei: " + this.getDbtype());
+			System.out.println("Datenbanknutzer aus XML Datei lesen: " + this.getUser());
+			System.out.println("Passwort aus XML Datei lesen: " + this.getPassword());
+			System.out.println("Datenbank Port aus XML Datei lesen: " + this.getPort());
+			System.out.println("SQL-Abfrage: " + this.getSqlStatement());
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		} catch (SAXException se) {
+			se.printStackTrace();
+		} catch (ParserConfigurationException pe) {
+			pe.printStackTrace();
+		}
+
+		return true;
+
+	}
+
+
+	public Connection getConnectionToDb() {
+		/* Auch hier redundaten Kode neutralisieren */
+		if (this.getDbtype().equals("mysql")) {
+			
+			try {
+				conToDb = DriverManager.getConnection("jdbc:mysql://" + this.localhost+ ":" + this.getPort()+ "/" + this.getDatabase() + "?" + "user="
+						+ this.getUser() + "&password=" + this.getPassword());
+
+			} catch (SQLException ex) {
+				// handle any errors
+				System.out.println("SQLException: " + ex.getMessage());
+				System.out.println("SQLState: " + ex.getSQLState());
+				System.out.println("VendorError: " + ex.getErrorCode());
+			}
+			return conToDb;
+		}
+		return null;
+
+
+	}
+	
+	
+	private String getValueOfXMLNode(Document document, String xmlNode){
+		String xmlNodeValue=xmlNode;
+		/* Die Methode funktioniert noch nicht */
+		
+		
+		NodeList nodeOfTag = document.getElementsByTagName(xmlNodeValue);
+		Element elementOfTag = (Element) nodeOfTag.item(0);
+		String valueOfElement = elementOfTag.getFirstChild().getTextContent();
+		return valueOfElement;
+	}
+
+}
---
+++ src/main/java/de/rib/exportcvs/ExportDbToCVS.java
@@ -0,0 +1,77 @@
+package de.rib.exportcvs;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+
+public class ExportDbToCVS {
+
+	public ExportDbToCVS() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String pathOfConfigFile = args[0];
+		boolean configFileExists = false;
+		/*
+		 * If first argument is an path to config file then use it. In other
+		 * case use default file
+		 */
+		if (args.length != 0) {
+			Path path = Paths.get(pathOfConfigFile);
+			System.out.println("Übergebener Pfad: " + pathOfConfigFile);
+			configFileExists = Files.exists(path, new LinkOption[] { LinkOption.NOFOLLOW_LINKS });
+			System.out.println("Config File Exists: " + configFileExists);
+		}
+		ConfigurationDBToCvs cDbToCvs = new ConfigurationDBToCvs();
+		if (configFileExists == true) {
+			System.out.println("Lese Config Datei: " + pathOfConfigFile);
+			cDbToCvs.readConfigFile(pathOfConfigFile);
+		}
+		Connection con = cDbToCvs.getConnectionToDb();
+		try {
+			System.out.println("Verbindung ist geschlossen: " + con.isClosed());
+			Statement statement = con.createStatement();
+			ResultSet rs = statement.executeQuery(cDbToCvs.getSqlStatement());
+			int colCount = rs.getMetaData().getColumnCount();
+			System.out.println("Spaltenanzahl des RS" + rs.getMetaData().getColumnCount());
+			
+			//FileWriter fileWriter = new FileWriter(cDbToCvs.getCsvfile());
+			Writer out = new BufferedWriter(new OutputStreamWriter(
+				    new FileOutputStream(cDbToCvs.getCsvfile()), "UTF-8"));
+			
+			//CSVFormat csvFormat = CSVFormat.newFormat(cDbToCvs.getDelimeter()).withRecordSeparator("\n").withHeader(rs).RFC4180;
+			CSVFormat csvFormat = CSVFormat.newFormat(cDbToCvs.getDelimeter()).withRecordSeparator("\n").withHeader(rs);
+			//CSVFormat csvFormat = CSVFormat.newFormat(cDbToCvs.getDelimeter()).withRecordSeparator("\n").MYSQL;
+			//CSVPrinter csvPrinter = new CSVPrinter(fileWriter, csvFormat);
+			//CSVFormat csvFormat = CSVFormat.MYSQL.withHeader(rs);
+			CSVPrinter csvPrinter = new CSVPrinter(out, csvFormat);
+			csvPrinter.printRecords(rs);
+			
+			out.flush();
+			out.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (IOException i) {
+			i.printStackTrace();
+		}
+
+	}
+
+}
